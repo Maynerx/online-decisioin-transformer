5ca0e3e912ed16dcc15abb9c830ae2a790eaf554
Mayner <madra.res94501@gmail.com>
2023-11-28 15:32:32 +0100

    Another fix

--- Custom_env.py
+++ Custom_env.py
@@ -29,10 +29,16 @@ class Env:
         self.selector = action_selector[action_select]
         self.rewards_scale = reward_scale
         self.use_mean = False
-        self.action_range = [
-            torch.tensor(0),
-            torch.tensor(self.env.action_space.n),
-        ]
+        try:
+            self.action_range = [
+                torch.tensor(0),
+                torch.tensor(self.env.action_space.n),
+            ]
+        except:
+            self.action_range = [
+                torch.tensor(self.env.action_space.low[0]),
+                torch.tensor(self.env.action_space.high[0]),
+            ]
         f_states, info = self.env.reset()
         self.states, self.action, self.rewards, self.timesteps, self.rtg = self._init_output(f_states)
         self._init_return()
